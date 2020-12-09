7e58547c0fb349da6c01866fa94308bbba20f15d
Vanessa Burton <vanessa.burton@ForestResearch.gov.uk>
2020-12-09 11:32:19 +0000

    initial commit from FR laptop

---
+++ FFMP_development.R
@@ -0,0 +1,10 @@
+
+# date: 09/12/20
+# author: VB
+# description: script to test the development of Future Forest Mangement Pathways (FFMPs) using
+# data provided by Skogforsk.
+
+wd <- "~/R/FFMPs"
+dirData <- paste0(wd,"/data-raw/")
+dirOut <- paste0(wd,"/data-processed/")
+
