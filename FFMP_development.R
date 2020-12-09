f78a7422e9b07ab1b16b004c29ef25bd08f745b4
Vanessa Burton <vanessa.burton@ForestResearch.gov.uk>
2020-12-09 11:37:16 +0000

    plan for analysis

--- FFMP_development.R
+++ FFMP_development.R
@@ -8,3 +8,21 @@ wd <- "~/R/FFMPs"
 dirData <- paste0(wd,"/data-raw/")
 dirOut <- paste0(wd,"/data-processed/")
 
+### explore seed zones --------------------------------------------------------------------------------
+
+
+### filter to 6 northern seed zones and simplify? -----------------------------------------------------
+
+
+### explore csv data (height, survival, performance) --------------------------------------------------
+
+
+### convert to raster ---------------------------------------------------------------------------------
+
+
+### test getting summary stats per seed zone ----------------------------------------------------------
+
+
+### test visualisation --------------------------------------------------------------------------------
+# potential to use this package https://bookdown.org/MathiasHarrer/Doing_Meta_Analysis_in_R/traffic-light-plots.html
+
