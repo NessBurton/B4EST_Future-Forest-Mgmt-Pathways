0365e1257320dad28a73588b166a2c7d5e1e9426
Vanessa Burton <vanessa.burton@forestresearch.gov.uk>
2020-12-14 15:26:50 +0000

    change approach - spatial join zones to csv data

--- FFMP_development.R
+++ FFMP_development.R
@@ -49,6 +49,8 @@ dfPerformance <- read.csv(paste0(dirData,"Productionpredictions/Refclimate_SO1.5
 head(dfPerformance)
 # note. explanations of the variables in .txt file in Productionpredictions folder
 
+
+
 ### convert to raster ---------------------------------------------------------------------------------
 
 # if locations are a regular grid
@@ -137,11 +139,34 @@ dfSeedZones <- extract(rstHeightLocal, shpSZ_sp, fun=mean, df=TRUE, na.rm=TRUE)
 # then set thresholds based on these values
 sdSeedZones <- extract(rstHeightLocal,shpSZ_sp,fun=sd,df=TRUE,na.rm=TRUE)
 
+### get elevation per zone to define -------------------------------------------
+
+rstElev <- rasterize(spPerformance, rst, spPerformance$GridAlt, fun=max)
+crs(rstElev)
+plot(rstElev)
+# now transform to utm, reprojection involves interpolation - default is bilinear
+utm <- crs(shpSZ)
+rstElev <- projectRaster(rstElev, crs = utm, res = 1000)
+plot(rstElev)
+dfElev <- extract(rstElev, shpSZ_sp, fun=max, df=TRUE, na.rm=TRUE)
+dfElev$Zone <- shpSZ_sp$ZON2
+
+ggplot(shpSZ_sf)+
+  geom_sf(aes(fill=ZON2))
+
+dfElev$elev <- rep(c("Upland","Lowland"),3)
+dfElev$location <- c("North","North","Central","Central","South","South")          
+dfElev$desc <- paste0(dfElev$location,"-",dfElev$elev)
+
+colnames(shpSZ_sf)[1] <- "Zone"
+shpSZ_sf <- left_join(shpSZ_sf,dfElev,by="Zone")
+ggplot(shpSZ_sf)+
+  geom_sf(aes(fill=desc))
+
 
 ### loop to do this for all GCMs -------------------------------------------------------------
 
 # just focus on RCP8.5 (all GCMs) compared to present
-
 
 # list production prediction files
 files <-  list.files(paste0(dirData, "Productionpredictions/"),pattern = "*.csv",full.names = T)
@@ -165,7 +190,7 @@ for (f in rcp85_files){
   
   #f <- rcp85_files[1]
   
-  scenario <- substring(f,75,89)
+  scenario <- substring(f,75,82)
   scenario_list[[length(scenario_list) + 1]] <- scenario
   
   print(paste0("Processing for scenario = ", scenario))
@@ -225,39 +250,9 @@ for (f in rcp85_files){
 }
 
 # check
-GCM_bc <- stack(paste0(dirOut,"SO_performance_bc85in50_SO1.5g.tif"))
-names(GCM_bc)
-spplot(GCM_bc)
-
-# to do:
-# a second loop or apply to extract values from each SO stack
-
-#write.csv(df_results, paste0(dirOut,"df_performance_PrProdidxSOh66.csv"))
-#write.csv(df_results, paste0(dirOut,"df_performance_PrProdidxSOh60.csv"))
-
-SOh66 <- read.csv(paste0(dirOut,"df_performance_PrProdidxSOh66.csv"))
-SOh66$seedOrchard <- "SOh66"
-SOh60 <- read.csv(paste0(dirOut,"df_performance_PrProdidxSOh60.csv"))
-SOh60$seedOrchard <- "SOh60"
-
-df_results <- rbind(SOh66,SOh60)
-df_results$X <- NULL
-
-df_results_lng <- df_results %>% 
-  pivot_longer(cols = 3:8, names_to="scenario",values_to="performance")
-
-df_results_lng <- df_results_lng %>% 
-  mutate(percChange = (performance - Refclimate_SO1.)/Refclimate_SO1. * 100)
-
-summary(df_results_lng$percChange)
-
-df_results_lng$change <- NA
-df_results_lng$change[which(df_results_lng$percChange<0)] <- "Small decline"
-df_results_lng$change[which(df_results_lng$percChange>0 
-                            & df_results_lng$percChange<=5)] <- "Small increase"
-df_results_lng$change[which(df_results_lng$percChange>5 
-                            & df_results_lng$percChange<=20)] <- "Increase"
-df_results_lng$change[which(df_results_lng$percChange>20)] <- "Large increase"
+check <- stack(paste0(dirOut,"SO_performance_bc85in50_SO1.5g.tif"))
+names(check)
+spplot(check)
 
 ### test visualisation --------------------------------------------------------------------------------
 
@@ -266,62 +261,152 @@ df_results_lng$change[which(df_results_lng$percChange>20)] <- "Large increase"
 #library(robvis)
 #rob_traffic_light(data = data_rob2, tool = "ROB2")
 
-df <- df_results_lng
-df$Zone <- factor(df$Zone, levels = zones)
-df$change <- factor(df$change, levels = c("Small decline","Small increase","Increase","Large increase"))
 
-# colour scale
-library(RColorBrewer)
-display.brewer.all()
-changeCols <- brewer.pal(4,"RdYlGn")
-names(changeCols) <- levels(df$change)
-colScale <- scale_colour_manual(name = "change",values = changeCols)
+### different approach - ignore rasters for now and just spatial join ---------------------------------
 
-df$dummyX <- "x"
+# work with just 6 northernmost zones
+# zones to focus on
+zones <- c("1a","1b","1c","2","3","7")
+shpSZ <- shpSZ %>% 
+  filter(ZON2 %in% zones == T)
 
-df <- left_join(df,dfElev,by="Zone")
+# dissolve/merge zones by ZON2 to simplify
+shpSZ_sf <- st_as_sf(shpSZ)
+shpSZ_sf$area <- st_area(shpSZ_sf) # add area to have a variable to be able to summarise
+shpSZ_sf <-
+  shpSZ_sf %>%
+  group_by(ZON2) %>% 
+  summarise(area = sum(area))
+plot(shpSZ_sf)
 
-df %>% 
-  filter(stat=="Pmin") %>% 
-  ggplot()+
-  geom_point(aes(seedOrchard,dummyX,size=percChange,col=change))+ coord_flip()+
-  #facet_wrap(~Zone, nrow = 7)+
-  facet_grid(desc~scenario)+
-  colScale +
-  ylab("Seed orchard choice")+
-  theme(axis.title.x=element_blank(),
-        axis.text.x=element_blank(),
-        axis.ticks.x=element_blank())
 
-df %>% 
-  ggplot()+
-  geom_boxplot(aes(seedOrchard,percChange))+ coord_flip()+
-  facet_grid(Zone~scenario)+
-  geom_vline(xintercept = 0)
+# refclimate
+df_ref <- read.csv(paste0(dirData,"Productionpredictions/Refclimate_SO1.5g_predictions.csv"))
+# create a spatialpoints dataframe
+sp_ref <- df_ref
+coordinates(sp_ref) <- ~ CenterLong + CenterLat
+# set crs - assume lat long
+proj4string(sp_ref) <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+# transform to utm
+sp_ref <- spTransform(sp_ref, CRSobj = utm )
+# convert to sf
+sf_ref <- st_as_sf(sp_ref)
+rm(sp_ref)
+# spatial join
+sf_ref <- st_join(sf_ref,shpSZ_sf)
+head(sf_ref)
+sf_ref$ZON2 <- factor(sf_ref$ZON2)
+summary(dataSF$ZON2)
 
-# aim for facet_wrap(Zone~scenario)
-# with x aes replaced by seed orchard
+sf_ref <- sf_ref[,15:19] %>% 
+  filter(!is.na(ZON2)) %>% # filter to just zones
+  st_drop_geometry() %>%
+  pivot_longer(1:4, names_to="seed_orchard",values_to="prod_idx")
+colnames(sf_ref)[3] <- "ref_prod_idx"
+rm(df_ref)
 
-### get elevation per zone to define -------------------------------------------
+# list production prediction files per scenario
+files <-  list.files(paste0(dirData, "Productionpredictions/"),pattern = "*.csv",full.names = T)
+# just RCP8.5 to test + current to compare
+files <- grep("85in50", files, value=TRUE)
+files
 
-rstElev <- rasterize(spPerformance, rst, spPerformance$GridAlt, fun=max)
-crs(rstElev)
-plot(rstElev)
-# now transform to utm, reprojection involves interpolation - default is bilinear
 utm <- crs(shpSZ)
-rstElev <- projectRaster(rstElev, crs = utm, res = 1000)
-plot(rstElev)
-dfElev <- extract(rstElev, shpSZ_sp, fun=max, df=TRUE, na.rm=TRUE)
-dfElev$Zone <- shpSZ_sp$ZON2
 
-ggplot(shpSZ_sf)+
-  geom_sf(aes(fill=ZON2))
+df_results_lng <- data_frame()
+df_results_summary <- data_frame()
+scenario_list <- c()
 
-dfElev$elev <- rep(c("Upland","Lowland"),3)
-dfElev$location <- c("North","North","Central","Central","South","South")          
-dfElev$desc <- paste0(dfElev$location,"-",dfElev$elev)
+for (f in files){
+  
+  #f <- rcp85_files[2]
+  
+  scenario <- substring(f,75,82)
+  scenario_list[[length(scenario_list) + 1]] <- scenario
+  
+  print("Read in data, convert to spatial, transform to utm")
+  dfP <- read.csv(f)
+  coordinates(dfP)<- ~ CenterLong + CenterLat
+  # set crs - assume lat long
+  proj4string(dfP) <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs") 
+  # transform to utm
+  dfP <- spTransform(dfP, CRSobj = utm )
+  dfP_sf <- st_as_sf(dfP)
+  rm(dfP)
+  
+  print("Spatial join to seed zones")
+  # spatial join
+  dfP_sf <- st_join(dfP_sf,shpSZ_sf)
+  #head(dfP_sf)
+  dfP_sf$ZON2 <- factor(dfP_sf$ZON2)
+  #summary(dfP_sf$ZON2)
+  
+  print("Transform to long format")
+  df_long <- dfP_sf[,17:21] %>% 
+    filter(!is.na(ZON2)) %>% # filter to just zones
+    st_drop_geometry() %>%
+    pivot_longer(1:4, names_to="seed_orchard",values_to="prod_idx")
+  
+  print("Add reference production")
+  # add ref production
+  df_long <- cbind(df_long,sf_ref$ref_prod_idx)
+  colnames(df_long)[4] <- "ref_prod"
+  
+  print("Calculate percentage change")
+  df_long <- df_long %>% 
+    mutate(perc_change = (prod_idx - ref_prod)/ref_prod * 100)
+  df_long$scenario <- scenario
+  
+  print("Add to results table")
+  df_results_lng <- rbind(df_results_lng, df_long)
+  
+  print("Calculate summaries")
+  df_summary <- df_long %>% 
+    group_by(ZON2, seed_orchard) %>% # group by zone
+    summarise_if(is.numeric,c("mean","sd","IQR","min","max")) #%>% 
+    #summarise(c("PrProdidxSOh60","PrProdidxSOh62","PrProdidxSOh64","PrProdidxSOh66"),.funs=c("mean","sd","IQR","min","max"))
+  df_summary$scenario <- scenario
+  
+  print("Add to results table")
+  df_results_summary <- rbind(df_results_summary, df_summary)
+  
+  print(paste0("Processed scenario: ",scenario))
+  
+}
 
-colnames(shpSZ_sf)[1] <- "Zone"
-shpSZ_sf <- left_join(shpSZ_sf,dfElev,by="Zone")
-ggplot(shpSZ_sf)+
-  geom_sf(aes(fill=desc))
+df_results_lng$change <- NA
+df_results_lng$change[which(df_results_lng$perc_change<0)]<-"decline"
+df_results_lng$change[which(df_results_lng$perc_change>0)] <- "increase"
+
+df_results_lng$seed.orchard <- substring(df_results_lng$seed_orchard,10,14)
+
+library(RColorBrewer)
+#brewer.pal(n = 8, name = "Dark2")
+pal <- c("#D95F02","#1B9E77")
+df_results_lng %>% 
+  filter(scenario != "MEAN85in") %>% 
+  ggplot()+
+  geom_boxplot(aes(seed.orchard,perc_change,col=change))+coord_flip()+
+  #scale_color_brewer(palette = "Dark2")+
+  scale_color_manual(values=pal)+  
+  facet_grid(ZON2~scenario)+
+  xlab("Seed orchard choice")+
+  ylab("Change in production index from reference climate (%)")
+
+df_results_lng %>% 
+  filter(scenario != "MEAN85in") %>% 
+  ggplot()+
+  geom_boxplot(aes(seed_orchard,perc_change,col=change))+coord_flip()+
+  facet_grid(rows="ZON2")
+
+df_results_summary$change <- NA
+df_results_summary$change[which(df_results_summary$perc_change_mean<0)]<-"decline"
+df_results_summary$change[which(df_results_summary$perc_change_mean>0)] <- "increase"
+
+df_results_summary %>% 
+  filter(scenario != "MEAN85in") %>% 
+  ggplot()+
+  geom_point(aes(seed_orchard,perc_change_mean,size=perc_change_mean,col=change))+coord_flip()+
+  facet_grid(ZON2~scenario)
+
+
