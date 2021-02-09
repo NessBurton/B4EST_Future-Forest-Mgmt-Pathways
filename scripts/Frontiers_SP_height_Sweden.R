33b1d03f0757ad8fb9252e0d350fff929218d68d
Vanessa Burton <vanessa.burton@forestresearch.gov.uk>
2021-02-09 11:42:22 +0000

    test/plot different menthods for calculating CoV

--- scripts/Frontiers_SP_height_Sweden.R
+++ scripts/Frontiers_SP_height_Sweden.R
@@ -1,10 +1,12 @@
 
-library(raster)
-library(rgdal)
-library(rgeos)
-library(rasterVis)
+# date: 09/02/21
+# authors: VB/FT
+# description: assess uncertainty in height predictions from Nordic Scots pine model
+# plot density distribution, identify outliers, calc z-scores, calc coefficient of variation
+
+### libs -----------------------------------------------------------------------
+
 library(dplyr)
-library(tidyverse)
 library(sf)
 library(sp)
 library(plyr)
@@ -15,7 +17,10 @@ library(RColorBrewer)
 library(rstatix)
 library(EnvStats)
 library(ggplot2)
+library(rnaturalearth)
+library(tidyverse)
 
+### dirs -----------------------------------------------------------------------
 
 wd <- "~/FFMPs" # sandbox
 dirData <- paste0(wd,"/data-raw/")
@@ -40,7 +45,7 @@ for (i in files){
   f <- read.csv(i)
   f$scenario <- scenario
   #head(f)
-  f <- f[,c("CenterLat","CenterLong","GridAlt","PrHeightLocal","PrHeightSOh60","PrHeightSOh62","PrHeightSOh64","PrHeightSOh66","scenario")]
+  f <- f[,c("GridID","CenterLat","CenterLong","GridAlt","PrHeightLocal","PrHeightSOh60","PrHeightSOh62","PrHeightSOh64","PrHeightSOh66","scenario")]
   dfPredictions <- rbind(dfPredictions,f)
   
 }
@@ -90,26 +95,29 @@ ggsave(GCMs_boxplots.all, file=paste0(dirFigs,"GCM_RCP_PrHeightLocal_boxplots_20
 
 ### identify outliers across all RCPs and GCMs ---------------------------------
 
-lsOutliers <- list()
-for(i in 4:8){
-  lsOutliers[[i]] <- boxplot(dfPredictions[[i]], plot=FALSE)$out 
-}
+# unsure about this bit
+
+#lsOutliers <- list()
+#for(i in 4:8){
+  #lsOutliers[[i]] <- boxplot(dfPredictions[[i]], plot=FALSE)$out 
+#}
 
 # identify the outliers within the dataset
-outliers.50 <- list()
-for(ii in 4:8){
-  outliers.50[[ii]] <- dfPredictions[dfPredictions[[ii]] %in% lsOutliers[[ii]],]
-}
+#outliers.50 <- list()
+#for(ii in 4:8){
+  #outliers.50[[ii]] <- dfPredictions[dfPredictions[[ii]] %in% lsOutliers[[ii]],]
+#}
 
-outliers.50[[4]]
+#outliers.50[[4]]
 
-# calculate z-scores and plot --------------------------------------------------
+### calculate z-scores and plot ------------------------------------------------
 
 # load country outline
 worldmap <- ne_countries(scale = 'medium', type = 'map_units',
                          returnclass = 'sf')
 sweden <- worldmap[worldmap$name == 'Sweden',]
 
+# use same cols as Felix
 nb.cols <- 9
 mycolors <- colorRampPalette(brewer.pal(9, "BrBG"))(nb.cols)
 
@@ -123,14 +131,17 @@ for (s in scenarios){
   #s <- scenarios[1]
   dfFilter <- dfPredictions %>% filter(scenario==s)
   dfFilter$Zscore_heightLocal <- spatialEco::outliers(dfFilter$PrHeightLocal)
-  # identify outliers
+  
+  # different method for identifying outliers
   dfOut <- dfFilter %>% 
     identify_outliers(PrHeightLocal) %>% 
     filter(is.outlier==TRUE | is.extreme==TRUE)
+  
   # make spatial
   spPredictions <- dfOut
   coordinates(spPredictions) <- ~CenterLong+CenterLat
   crs(spPredictions) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0" 
+  
   # convert to sf for plotting
   sfPredictions <- st_as_sf(spPredictions)
   p1 <- ggplot()+
@@ -145,109 +156,109 @@ for (s in scenarios){
   
 }
 
-### calculate CV ---------------------------------------------------------------
+### calculate CoV --------------------------------------------------------------
 
-# will mean for entire df be same as ensemble mean?
+# aim of CoV
+# show variation per GCM in relation to the mean for each RCP/ensemble mean result
 
-test <- dfPredictions[,c(4,10,11)] %>% 
-  filter(RCP =="4.5")
+# unsure if calculating the mean per RCP/GCM is the same as using the ensemble model results...
+# ...and calculate CoV in relation to that
+# check and see..
 
-resample::bootstrap(test, statistic = mean(test$PrHeightLocal), R=10)
+# take 100 random samples of 1000 data points
+dfRandom <- bind_rows(replicate(100, dfPredictions %>% sample_n(1000), simplify=F), .id="Obs")
+dfRandom$RCP <- as.factor(dfRandom$RCP)
+dfRandom$Obs <- as.factor(dfRandom$Obs)
 
-# against ensemble mean?
+# calculate the mean height for each RCP (across GCMs), for each random replicate
+dfRCP <- dfRandom[,c("RCP","GCM","Obs","PrHeightLocal","ensembleHeight")] %>% 
+  dplyr::group_by(RCP, Obs) %>% 
+  dplyr::mutate(RCP.mean = mean(PrHeightLocal),
+                RCP.diff2 = (PrHeightLocal-RCP.mean)^2) %>%
+  ungroup() %>% 
+  # then for each GCM (within RCP & replicate), calc standard deviation against the RCP mean
+  dplyr::group_by(RCP,GCM,Obs) %>% 
+  dplyr::summarise(RCP.mean = unique(RCP.mean),
+                   GCM.sd= sqrt(sum(RCP.diff2)/1000)) %>% 
+  mutate(CoV = GCM.sd/RCP.mean*100)
 
-ensembleFiles <- list.files(paste0(dirData, "Productionpredictions/"),pattern = "*.csv",full.names = T)
-ensembleFiles <- grep("MEAN", ensembleFiles, value=TRUE)
-
-dfEnsemble <- tibble()
-for (i in ensembleFiles){
-  
-  scenario <- strsplit(i, "[_]")[[1]][1]
-  scenario <- strsplit(scenario, "[/]")[[1]][8]
-  f <- read.csv(i)
-  f$scenario <- scenario
-  #head(f)
-  f <- f[,c("CenterLat","CenterLong","GridAlt","PrHeightLocal","PrHeightSOh60","PrHeightSOh62","PrHeightSOh64","PrHeightSOh66","scenario")]
-  dfEnsemble <- rbind(dfEnsemble,f)
-  
-}
-
-rcps <- c("45in50","85in50")
-
-head(dfPredictions)
-head(dfEnsemble)
-
-gcmList <- unique(dfPredictions$scenario)
-rsamp <- 10000
-dfCoV <- tibble()
-
-library(sjstats) # for bootstrap
-
-# loop to calc CV against ensemble mean per GCM/RCP combo
-for (gcm in gcmList){
-  
-  #gcm <- gcmList[2]
-  dfGCM <- dfPredictions %>% filter(scenario %in% gcm == TRUE)
-  
-  if (length(grep("45in50",gcm))>1){
-    dfMean <- dfEnsemble %>% filter(scenario=="MEAN45in50")
-  }else{
-    dfMean <- dfEnsemble %>% filter(scenario=="MEAN85in50")
-  }
-  
-  dfCV <- tibble(dfGCM$scenario,as.numeric(dfGCM$PrHeightLocal),as.numeric(dfMean$PrHeightLocal))
-  colnames(dfCV) <- c("Scenario","height.GCM","height.Ensemble")
-  
-  boots <- bootstrap(dfCV, 10, rsamp)
-  
-  
-  for (i in resamples){
-    
-    dfCV <- resamples[1] %>% 
-      mutate(mean.GCM = mean(height.GCM),
-             mean.Ensemble = mean(height.Ensemble),
-             sd.GCM = sd(height.GCM),
-             sd.Ensemble = sqrt(sum((height.GCM-mean.Ensemble)^2)/rsamp),
-             CV.GCM = sd.GCM/mean.GCM*100,
-             CV.Ensemble = sd.Ensemble/mean.Ensemble*100)
-    
-  }
-  dfCV <- dfCV %>% 
-    #sample_n(.,rsamp) %>% # randomly sample
-    mutate(mean.GCM = mean(height.GCM),
-           mean.Ensemble = mean(height.Ensemble),
-           sd.GCM = sd(height.GCM),
-           sd.Ensemble = sqrt(sum((height.GCM-mean.Ensemble)^2)/rsamp),
-           CV.GCM = sd.GCM/mean.GCM*100,
-           CV.Ensemble = sd.Ensemble/mean.Ensemble*100)
-  
-  dfCoV <- rbind(dfCoV,dfCV)
-  
-}
-
-summary(dfCoV)
-dfCoV$RCP <- ifelse(grepl("45in50", dfCoV$Scenario), 'RCP 4.5',
-                            ifelse(grepl("85in50", dfCoV$Scenario), 'RCP 8.5', 'RCP_all'))
-dfCoV$GCM <- ifelse(grepl("bc", dfCoV$Scenario), 'bc - BCC-CSM1-1',
-                            ifelse(grepl("he", dfCoV$Scenario), 'he - HadGEM2-ES',
-                                   ifelse(grepl("mg", dfCoV$Scenario), 'mg - MRI-CGCM3',
-                                          ifelse(grepl("mi", dfCoV$Scenario), 'mi - MIROC-ESM-CHEM',
-                                                 ifelse(grepl("no", dfCoV$Scenario), 'no - NorESM1-M', 'GCM_all')))))
-
-ggplot(dfCoV)+
-  geom_point(aes(GCM, CV.Ensemble,col=GCM))+
-  ylim(0,50)+ylab("CoV vs. ensemble mean (%)")+
+# boxplot to show results from random replicates
+(CV1 <- ggplot(dfRCP)+
+  geom_boxplot(aes(GCM, CoV,col=GCM))+
+  ylim(0,50)+ylab("CoV (%)")+
   facet_wrap(~RCP)+
   theme_bw()+
+  ggtitle("PrHeightLocal CoV (%) vs RCP mean height")+
   theme(axis.title.x = element_blank(),
         axis.text.x = element_blank(),
-        axis.ticks = element_blank())
+        axis.ticks = element_blank()))
+ggsave(CV1, file=paste0(dirFigs, "PrHeightLocal_CoV_vs_RCP_mean.png"), width=10, height=6, dpi=300)
+# pretty much identical to results comparing against ensemble mean below
+# so i think fine to use either?
 
-ggplot(dfCoV)+
-  geom_point(aes(GCM, CV.GCM,col=GCM))+
-  ylim(0,50)+ylab("CoV vs. GCM mean (%)")+
+# read in ensemble results
+files <- list.files(paste0(dirData, "Productionpredictions/"),pattern = "*.csv",full.names = T)
+ensembleFiles <- grep("MEAN", files, value=TRUE)
+
+df4.5_ensemble <- read.csv(ensembleFiles[1])
+df8.5_ensemble <- read.csv(ensembleFiles[2])
+
+df4.5_ensemble <- df4.5_ensemble[,c("GridID","PrHeightLocal")]
+colnames(df4.5_ensemble)[2] <- "HeightLocal_ENS45"
+df8.5_ensemble <- df8.5_ensemble[,c("GridID","PrHeightLocal")]
+colnames(df8.5_ensemble)[2] <- "HeightLocal_ENS85"
+
+dfRandom <- left_join(dfRandom,df4.5_ensemble,by="GridID")
+dfRandom <- left_join(dfRandom,df8.5_ensemble,by="GridID")
+
+dfRandom$ensembleHeight <- NA
+dfRandom$ensembleHeight[which(dfRandom$RCP=="4.5")]<-dfRandom$HeightLocal_ENS45[which(dfRandom$RCP=="4.5")]
+dfRandom$ensembleHeight[which(dfRandom$RCP=="8.5")]<-dfRandom$HeightLocal_ENS85[which(dfRandom$RCP=="8.5")]
+
+head(dfRandom)
+
+ENSmean45 <- mean(df4.5_ensemble$HeightLocal_ENS45)
+ENSmean85 <- mean(df8.5_ensemble$HeightLocal_ENS85)
+
+# calculate CoV against ensemble model results mean
+# manually calc sd against each ensemble mean, not mean of PrHeightLocal
+dfENS <- dfRandom %>% 
+  dplyr::group_by(RCP,GCM,Obs) %>% 
+  dplyr::summarise(ENSmean = mean(ensembleHeight),
+                   ENSsd1 = sqrt(sum((PrHeightLocal-ENSmean)^2)/1000),
+                   ENSsd2 = if (RCP=="4.5"){sqrt(sum((PrHeightLocal-ENSmean45)^2)/1000)}
+                   else{ sqrt(sum((PrHeightLocal-ENSmean85)^2)/1000)}) %>% # manually calc sd against ensemble mean, not mean of PrHeightLocal
+  mutate(CoV1 = ENSsd1/ENSmean*100,
+         CoV2 = if (RCP=="4.5"){ENSsd2/ENSmean45*100}else{ENSsd2/ENSmean85*100})
+
+(CV2 <- ggplot(dfENS)+
+  geom_boxplot(aes(GCM, CoV1,col=GCM))+
+  ylim(0,50)+ylab("CoV (%)")+
   facet_wrap(~RCP)+
   theme_bw()+
+  ggtitle("PrHeightLocal CoV (%) vs. ensemble mean height")+
   theme(axis.title.x = element_blank(),
         axis.text.x = element_blank(),
-        axis.ticks = element_blank())
+        axis.ticks = element_blank()))
+ggsave(CV2, file=paste0(dirFigs, "PrHeightLocal_CoV_vs_ensemble_mean.png"), width=10, height=6, dpi=300)
+
+# or can it be this simple?
+# calculate mean height for each GCM within each RCP, for each random replicate
+dfGCM <- dfRandom %>% 
+  dplyr::group_by(RCP, GCM, Obs) %>% 
+  dplyr::summarise(GCM.mean = mean(PrHeightLocal),
+                   GCM.sd = sd(PrHeightLocal)) %>% 
+  mutate(CoV = GCM.sd/GCM.mean*100)
+
+# boxplot to show results from random replicates
+(CV3 <- ggplot(dfGCM)+
+  geom_boxplot(aes(GCM, CoV,col=GCM))+
+  ylim(0,50)+ylab("CoV (%)")+
+  facet_wrap(~RCP)+
+  theme_bw()+
+  ggtitle("PrHeightLocal CoV (%) vs. GCM mean")+
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks = element_blank()))
+ggsave(CV3, file=paste0(dirFigs, "PrHeightLocal_CoV_vs_GCM_mean.png"), width=10, height=6, dpi=300)
+
